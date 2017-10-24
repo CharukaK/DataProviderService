5620dbb4ba46c066a2e91c177e6b4c4db35e2daf
Charuka Karunanayake <charukakarunanayake@gmail.com>
2017-10-24 11:02:41 +0530

    RDBMS polling rest API

--- src/main/java/org/wso2/service/GenerationAPI.java
+++ src/main/java/org/wso2/service/GenerationAPI.java
@@ -16,11 +16,17 @@
 package org.wso2.service;
 
 
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -68,9 +74,41 @@ public class GenerationAPI {
     @POST
     @Path("/poll-db")
     public String pollDb(String message){
+        int lastRow=Integer.parseInt(message.split(";")[1]);
+        DataSetMetadata metadata = null;
+        Object[][] data = new Object[0][];
+        RDBMSProviderConf conf=new Gson().fromJson(message.split(";")[0],RDBMSProviderConf.class);
+        try {
+            Connection conn=RDBMSProvider.getConnection(conf.getUrl(),conf.getUsername(),conf.getPassword());
+            Statement statement=conn.createStatement();
+            ResultSet resultSet=statement.executeQuery(conf.getQuery());
+            ResultSetMetaData rsmd=resultSet.getMetaData();
+            metadata= new DataSetMetadata(rsmd.getColumnCount());
+            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                metadata.put(i - 1, rsmd.getColumnName(i),
+                        RDBMSProvider.getMetadataTypes(conf.getUrl().split(":")[1], rsmd.getColumnTypeName(i)));
+            }
+            resultSet.last();
+            data = new Object[resultSet.getRow() - lastRow][metadata.getNames().length];
+            resultSet.absolute(lastRow);
+            while (resultSet.next()){
+                for (int i = 0; i < metadata.getNames().length; i++) {
+                    if(metadata.getTypes()[i].equalsIgnoreCase("linear")){
+                        data[resultSet.getRow()-lastRow -1][i]=resultSet.getDouble(i+1);
+                    }else if(metadata.getTypes()[i].equalsIgnoreCase("ordinal")){
+                        data[resultSet.getRow()-lastRow -1][i]=resultSet.getString(i+1);
+                    }else {
+                        data[resultSet.getRow()-lastRow -1][i]=resultSet.getDate(i+1);
+                    }
+                }
+            }
 
-        System.out.println(message);
-        return "hello";
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return "metadata;"+new Gson().toJson(metadata)+"|data;"+new Gson().toJson(data);
     }
 
 
